713359c07eb8ce4c7f4b8e0afe68142cc58efed5
Karencheenn <karen.chenlai@mail.mcgill.ca>
2024-01-20 12:47:28 -0500

    [code] addition of player button control

--- A Life/Assets/Scripts/StudentController.cs
+++ A Life/Assets/Scripts/StudentController.cs
@@ -14,6 +14,7 @@ public class StudentController : MonoBehaviour
     float horizontal;
     float vertical;
     private int maxStamina = 100;
+    private Building nearbyBuilding = null;
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +53,39 @@ public class StudentController : MonoBehaviour
         {
             return 100;
         }
+
+    // display task if building is nearby and 'E' is pressed
+    if (Input.GetKeyDown(KeyCode.E) && nearbyBuilding!= null) {
+        nearbyBuilding.displayTask();
+    }
+
+
+    // hide task if 'X' is pressed
+    if (Input.GetKeyDown(KeyCode.X) && nearbyBuilding!= null) {
+        nearbyBuilding.HideTask();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Building"))
+        {
+            Building building = other.gameObject.GetComponent<Building>();
+            if (building != null)
+            {
+                // Set nearbyBuilding if the student is close enough to the building
+                nearbyBuilding = building;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Building") && nearbyBuilding == other.gameObject.GetComponent<Building>())
+        {
+            // Clear nearbyBuilding if the student is no longer close to the building
+            nearbyBuilding = null;
+        }
+    }
     }
 
     void FixedUpdate()
@@ -62,6 +96,4 @@ public class StudentController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
-
-    //hello
 }
