f941c74cf828c503e190369caf0fb2f3b8a4ce8d
Karencheenn <karen.chenlai@mail.mcgill.ca>
2024-01-20 11:57:15 -0500

    [code] setting key button for nearbyBuilding

--- A Life/Assets/Scripts/StudentController.cs
+++ A Life/Assets/Scripts/StudentController.cs
@@ -12,6 +12,7 @@ public class StudentController : MonoBehaviour
 
     float horizontal;
     float vertical;
+    private Building nearbyBuilding = null;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,17 @@ public class StudentController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        // display task if building is nearby and 'E' is pressed
+        if (Input.GetKeyDown(KeyCode.E) && nearbyBuilding!= null) {
+            nearbyBuilding.displayTask();
+        }
+
+
+        // hide task if 'X' is pressed
+        if (Input.GetKeyDown(KeyCode.X) && nearbyBuilding!= null) {
+            nearbyBuilding.HideTask();
+        }
     }
 
     void FixedUpdate()
@@ -35,5 +47,26 @@ public class StudentController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    //hello
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Building"))
+        {
+            Building building = other.gameObject.GetComponent<Building>();
+            if (building != null)
+            {
+                // Set nearbyBuilding if the student is close enough to the building
+                nearbyBuilding = building;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Building") && nearbyBuilding == other.gameObject.GetComponent<Building>())
+        {
+            // Clear nearbyBuilding if the student is no longer close to the building
+            nearbyBuilding = null;
+        }
+    }
+
 }
