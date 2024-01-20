9c6e82af023d0f33f7365d1c3a7d881da4afbaca
Uffy-oo <yufei.li2@mail.mcgill.ca>
2024-01-21 01:40:30 +0800

    Activity: Text Trigger

---
+++ A Life/Assets/Scripts/TextAssignment.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogBoxText;
+    public string activityText;
+    private bool isPlayerInside;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && isPlayerInside)
+        {
+            dialogBox.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        isPlayerInside = true;
+    }
+
+    void OnTriggerExit2D(Collider other)
+    {
+        isPlayerInside = false;
+        dialogBox.SetActive(false);
+    }
+}
