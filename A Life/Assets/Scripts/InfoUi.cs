d74e0e5c1c390eba212478b17fe7ae5e903ef43f
Yihong-anthony <li.yihong2653@icloud.com>
2024-01-21 18:22:09 -0500

    Update InfoUi.cs

--- A Life/Assets/Scripts/InfoUi.cs
+++ A Life/Assets/Scripts/InfoUi.cs
@@ -13,6 +13,19 @@ public class InfoUi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CloseInfoUI();
+    }
+
+    public void CloseInfoUI()
+    {
+        // press ESC to exit game info
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            GameObject menu = GameObject.FindGameObjectWithTag("menuTest").transform.gameObject;
+            GameObject GameInfoUI = GameObject.FindGameObjectWithTag("gameInfoUI").transform.gameObject;
+            menu.transform.GetChild(0).gameObject.SetActive(true);
+            GameInfoUI.transform.GetChild(0).gameObject.SetActive(false);
+        }
+
     }
 }
